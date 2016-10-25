96dcf4e5ad0235cc60274a8f2a87354cc44c2e90
Jeremy Warner <jeremywrnr@gmail.com>
2016-10-24 20:18:10 -0700

    add map

--- client/main.js
+++ client/main.js
@@ -5,7 +5,9 @@ import { Posts } from '../imports/posts.js';
 Template.hello.helpers({
   posts() {
     // time sort the posts and then return
-    return Posts.find({}, {sort: {time: -1}});
+    return Posts.find({}, {sort: {time: -1}})
+      .map( s => s ) ;
+      //.map( s => new Date(s).toString() );
   },
 });
 
