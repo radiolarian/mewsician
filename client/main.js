e48ed978ca87f0e1942dc6c7c24d6b47dfabb901
Jeremy Warner <jeremywrnr@gmail.com>
2016-10-24 20:11:15 -0700

    adding post endpoint

--- client/main.js
+++ client/main.js
@@ -1,22 +1,11 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import { Posts } from '../imports/posts.js';
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+  posts() {
+    // time sort the posts and then return
+    return Posts.find({}, {sort: {time: -1}});
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+import '../imports/posts.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
