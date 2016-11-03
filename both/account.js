81d7a363eb5bc73f49971396d2f70852adac9841
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-03 01:08:23 -0700

    adding gcloud and account info, breaking!

---
+++ both/account.js
@@ -0,0 +1,15 @@
+// Ensuring every user has an email address, should be in server-side code
+Accounts.validateNewUser((user) => {
+  new SimpleSchema({
+    _id: { type: String },
+    emails: { type: Array },
+    'emails.$': { type: Object },
+    'emails.$.address': { type: String },
+    'emails.$.verified': { type: Boolean },
+    createdAt: { type: Date },
+    services: { type: Object, blackbox: true }
+  }).validate(user);
+
+  // Return true to allow user creation to proceed
+  return true;
+});
