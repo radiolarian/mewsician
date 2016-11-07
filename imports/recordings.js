2dcfc63aba4c5d3064e53856458ce218ee03c468
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-06 20:00:33 -0800

    adding recordings collection

---
+++ imports/recordings.js
@@ -0,0 +1,8 @@
+// Recordings from the device
+
+export const Recordings = new Mongo.Collection('recordings');
+  /* schema:
+   * - uid - user indentification number
+   * - rid - recording identification num
+   **/
+
