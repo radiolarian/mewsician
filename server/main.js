1384402317c09da5cb60b44a7aca7d7b284c14d5
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-07 00:09:13 -0800

    no autopublish

--- server/main.js
+++ server/main.js
@@ -3,3 +3,7 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.publish('files.music.all', function () {
+  return Music.find().cursor;
+});
