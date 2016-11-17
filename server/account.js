55da5a0845cdfe5b92165aee77df0a950b1b97ab
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-17 15:21:03 -0800

    merge meteor methods

--- server/account.js
+++ server/account.js
@@ -1,27 +1,6 @@
 // generating and updating keys for users
 // https://themeteorchef.com/recipes/writing-an-api/
 
-Meteor.methods({
-  regenerateKey(uid) {
-    if (!Meteor.users.findOne(uid))
-      return; // this isnt a real user
-
-    // generate a new auth key for user
-    var newKey = Random.hexString(32);
-
-    try {
-      var keyId = ChipAuth.upsert({ "user": uid }, {
-        $set: {
-          "key": newKey
-        }
-      });
-      return keyId;
-    } catch(e) {
-      return e;
-    }
-  },
-});
-
 // Ensuring every user has an email address, should be in server-side code
 
 Accounts.validateNewUser((user) => {
--- server/data.js
+++ server/data.js
@@ -2,9 +2,28 @@
 
 Meteor.methods({
   removeFile(id) {
-    console.log("removing", id)
-    var file = Music.findOne(id)
-    console.log(file)
-    Music.remove({_id: id});
+    var file = Music.findOne(id);
+    if (file) Music.remove({_id: id});
+  },
+
+  // generating and updating keys for users
+  // https://themeteorchef.com/recipes/writing-an-api/
+  regenerateKey(uid) {
+    if (!Meteor.users.findOne(uid))
+      return; // this isnt a real user
+
+    // generate a new auth key for user
+    var newKey = Random.hexString(32);
+
+    try {
+      var keyId = ChipAuth.upsert({ "user": uid }, {
+        $set: {
+          "key": newKey
+        }
+      });
+      return keyId;
+    } catch(e) {
+      return e;
+    }
   },
 });
