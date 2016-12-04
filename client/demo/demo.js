a14a0b97e629bf1244b842a564dc3b0a3f74a361
Jeremy Warner <jeremywrnr@gmail.com>
2016-12-03 16:02:09 -0800

    remove demo

--- client/demo/demo.js
+++
@@ -1,8 +0,0 @@
-import { Template } from 'meteor/templating';
-import { Posts } from '../../both/posts.js';
-
-Template.demo.helpers({
-  posts() { // time sort the posts and then return
-    return Posts.find({}, {sort: {time: -1}});
-  },
-});
